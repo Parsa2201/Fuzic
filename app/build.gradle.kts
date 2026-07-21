be47de28ada9dee2c3ef786ae23a4a7936b3e714
Parsa Salamatipour <sa.parsa20@gmail.com>
2026-07-22 01:29:44 +0330

    build(ui): add paging compose support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.paging.common.ktx)
+    implementation(libs.androidx.paging.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
